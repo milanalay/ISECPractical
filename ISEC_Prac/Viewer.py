c347579ba6fa0444c3a5583d53e1f9f596269acf
milanalay <milan.metashrine@gmail.com>
2023-04-16 20:50:31 +0800

    first commit for developer 1

--- ISEC_Prac/Viewer.py
+++ ISEC_Prac/Viewer.py
@@ -10,7 +10,7 @@ def view(grid):
                 print("  ", end = "")
                     
             elif grid[i][j] == Maze.WALL:
-                print("##", end = "")
+                print("@@", end = "")
                     
             elif grid[i][j] == Maze.START:
                 print("^^", end = "")
